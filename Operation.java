6932ea524848d9d14b28c6e09dd66a473b13e75b
Altan <altan.altay@ug.bilkent.edu.tr>
2024-02-06 13:53:20 +0300

    findMax and findMin methods added.

--- Operation.java
+++ Operation.java
@@ -2,9 +2,11 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Operation{
+    
     private static int[] array;
     private static Random rand;
     private static Scanner scanner;
+    
     public static void main(String[] args) {
         scanner = new Scanner(System.in);
         rand = new Random();
@@ -20,14 +22,47 @@ public class Operation{
 
         
     }
+    
+    /**
+     * finds the min element and prints it.
+     */
+    public static void findMin()
+    {
+        int min = array[0];
+        for (int i = 1; i < array.length; i++)
+        {
+            if (array[i] < array[0])
+            {
+                min = array[i];
+            }
 
-    public static void findMaxMin(){
+            System.out.println("Min number is: " + min);
+        }
+    }
+    
+    /**
+     * finds the max element and prints it
+     */
+    public static void findMax()
+    {
+        int max = array[0];
+        for (int i = 1; i < array.length; i++)
+        {
+            if (array[i] > array[0])
+            {
+                max = array[i];
+            }
+
+            System.out.println("Max number is: " + max);
+        }
+    }
+
+    public static void findAvarage()
+    {
 
     }
-    public static void findAvarage(){
-
-    }
-    public static void findSumOfElements(){
+    public static void findSumOfElements()
+    {
 
     }
 
